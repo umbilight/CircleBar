13c3b745bb42ebd657f81523360724f9474cc5f0
umbilight <83448058+umbilight@users.noreply.github.com>
2021-05-07 16:07:55 +0200

    first commit

---
+++ Example/CircleBar/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  CircleBar
+//
+//  Created by Umberto Casa on 07/05/21.
+//  Copyright (c) 2021 Umberto Casa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
